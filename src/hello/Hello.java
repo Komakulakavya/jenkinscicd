f330c664080e6bf1eb8acd25f3175b1d7d27113a
Komakulakavya <125781517+Komakulakavya@users.noreply.github.com>
2025-09-11 19:08:29 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,13 +1,6 @@
-> package hello;
-> public class Hello {
->   public static void main(String[] args) {
->     System.out.println("Hello from Pipeline CI!");
->   }
-> }
-> ^[[200~cat > src/hello/Hello.java << 'EOF'
 package hello;
 public class Hello {
-  public static void main(String[] args) {
-    System.out.println("Hello from Pipeline CI!");
-  }
+   public static void main(String[] args) {
+     System.out.println("Hello from Pipeline CI!");
+   }
 }
